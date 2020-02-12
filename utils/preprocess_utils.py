42774640ffa2f7ce467378f70e92359c5f604581
lucasthim <lucasthim@yahoo.com>
2020-02-12 00:02:02 -0300

    Fix in plotting categorical distribution and method abstraction

---
+++ utils/preprocess_utils.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import scipy.stats as sp
+import numpy as np
+
+def remove_outliers(df,columns_to_analyse,threshold = 4):
+    z_score = np.abs(sp.zscore(df[columns_to_analyse]))
+    df_no_outlier = df.loc[(z_score < threshold).all(axis=1),:].copy()
+    print(df_no_outlier.shape)
+    return df_no_outlier
